faeacdfd43816c5e26d222d0a05f9b10688b29da
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2015-10-25 11:44:42 -0400

    Finished the side-nav so it's fixed and more fluidly responsive.

--- js/components/aside.js
+++ js/components/aside.js
@@ -1,7 +1,7 @@
 $('.aside-button').on('click', function(){
 	
 	if ($(window).width() < 787) {
-	   $('aside').toggleClass('show-aside');
+	   $('.side-content').toggleClass('show-aside');
 	   $('.aside-button').toggleClass('aside-button-move');
 	}
 });
